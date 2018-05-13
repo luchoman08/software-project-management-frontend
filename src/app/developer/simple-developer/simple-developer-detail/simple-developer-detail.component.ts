14ee32659fb0e27f8b3133820a677da6f27a0c43
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-13 14:31:48 -0500

    Edit and instanciate available hours per week to a developer

--- src/app/developer/simple-developer/simple-developer-detail/simple-developer-detail.component.ts
+++ src/app/developer/simple-developer/simple-developer-detail/simple-developer-detail.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SimpleDeveloper } from '../../../core/models/simple-assignment/simple-developer';
-
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import * as mocksSimpleDevels from '../../../mocks/simple-developers';
 import { SIMPLEDEVELS } from '../../../mocks/simple-developers';
-
+import { SingleDataDialogEditOrAddComponent } from '../../../page-components/common-components';
 @Component({
   selector: 'app-simple-developer-detail',
   templateUrl: './simple-developer-detail.component.html',
@@ -12,8 +12,22 @@ import { SIMPLEDEVELS } from '../../../mocks/simple-developers';
 export class SimpleDeveloperDetailComponent implements OnInit {
   color = '#ff8d15';
   simpleDeveloper: SimpleDeveloper = SIMPLEDEVELS[0] ;
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+  ) { }
 
+  openEditAvailableHoursPerWeekDialog(): void {
+    const dialogRef = this.dialog.open(SingleDataDialogEditOrAddComponent, {
+      width: '320px',
+      data: {
+        value: this.simpleDeveloper.available_hours_per_week,
+        valueName: 'horas disponibles a la semana',
+        type: 'number'}
+    });
+    dialogRef.afterClosed().subscribe(value => {
+      this.simpleDeveloper.available_hours_per_week = value;
+    });
+  }
   ngOnInit() {
   }
 
