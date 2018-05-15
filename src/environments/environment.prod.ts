d8c5f9d62e3789aeec04950649beded297b894fb
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-14 20:39:04 -0500

    zoho api url added to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_taiga_url: 'https://api.taiga.io/api/v1'
+  api_taiga_url: 'https://api.taiga.io/api/v1',
+  api_zoho_url: 'https://projectsapi.zoho.com/restapi'
 };
