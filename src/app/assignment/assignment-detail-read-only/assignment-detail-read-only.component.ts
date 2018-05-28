660b050c98860c8a9fe1d0997526893cdc50b304
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-28 11:14:24 -0500

    Spinner with information of user percentage of each developer added

--- src/app/assignment/assignment-detail-read-only/assignment-detail-read-only.component.ts
+++ src/app/assignment/assignment-detail-read-only/assignment-detail-read-only.component.ts
@@ -4,23 +4,40 @@ import { SIMPLEUSERSTORIES } from '../../mocks/simple-mocks/simple-user-stories'
 import { MatDialog } from '@angular/material';
 import { DeveloperSelectDialogComponent } from '../../developer/developer-select-dialog/developer-select-dialog.component';
 import { Developer } from '../../core/models';
+import { ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-assignment-detail-read-only',
   templateUrl: './assignment-detail-read-only.component.html',
-  styleUrls: ['./assignment-detail-read-only.component.scss']
+  styleUrls: ['./assignment-detail-read-only.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AssignmentDetailReadOnlyComponent implements OnInit {
   simpleAssignment: SimpleAssignmentOutput;
   selectedUserStory: UserStory;
   selectedDeveloper: Developer;
+  mode ;
+  value;
   developers: Developer[];
   userStories: UserStory[];
   constructor(
-    public dialog: MatDialog,
+    public dialog: MatDialog
   ) {
     this.simpleAssignment = JSON.parse('{"assignmentErrors":[],"simpleDeveloperUserStoriesAssigned":[{"developer":{"id":788273,"available_hours_per_week":45,"full_name":"Luis Gerardo Manrique Cardona"},"userStories":[{"id":2239865,"total_points":12,"reference":1,"subject":"Permitir el logueo en el sistema basado en los usuarios de el Campus Virtual, haciendo uso de sus nombres de usuario, contraseña y rol asignado en el Campus"},{"id":2252677,"total_points":12,"reference":2,"subject":"Permitir asignaciones basadas en un conjunto de historias resueltas"}]},{"developer":{"id":8,"available_hours_per_week":45,"full_name":"Arnaldo ramirez"},"userStories":[{"id":5,"total_points":12,"reference":4,"subject":"Permitir reasignar historias de usuario"},{"id":18,"total_points":12,"reference":5,"subject":"Permitir reasignacion en tiempo  real"}]}]}');
     this.developers = new Array<Developer>();
+    this.mode = 'determinate';
+    this.value = 40;
     this.userStories = new Array<UserStory>();
+  }
+  totalUserStoryPoints(userStories: UserStory[]): number {
+    let totalPoints: number = 0;
+    for(let userStory of userStories) {
+      totalPoints += userStory.total_points;
+    }
+    return totalPoints;
+  }
+  developerPercentageOcupation(developerAssignment: DeveloperUserStoriesAssigned) {
+    console.log("una vez y otra mas hasta juntos terminar ");
+    return  this.totalUserStoryPoints(developerAssignment.userStories) * 100 / developerAssignment.developer.available_hours_per_week;
   }
   openSelectDevelopers(userStory: UserStory, developers: Developer[]): void {
     this.selectedUserStory = userStory;
@@ -47,8 +64,7 @@ export class AssignmentDetailReadOnlyComponent implements OnInit {
         }
       )[0];
       let indexUserStoryChanged: number = developersAssginmentToDisassign.userStories.indexOf(this.selectedUserStory);
-      const userStoryToReasign: UserStory = developersAssginmentToDisassign.userStories[indexUserStoryChanged];
-      developersAssginmentToAssign.userStories.push(userStoryToReasign);
+      developersAssginmentToAssign.userStories.push(this.selectedUserStory);
       developersAssginmentToDisassign.userStories.splice(indexUserStoryChanged, 1);
       this.selectedUserStory.assignedUser = selectedDeveloper;
     });
