77269a5475391c6a80159d8df19a62b8dee0b7d1
Luis Gerardo Manrique <luis.manrique@correounivalle.edu.co>
2018-05-28 15:18:40 -0500

    Non necesary mode and value vars

--- src/app/assignment/assignment-detail-read-only/assignment-detail-read-only.component.ts
+++ src/app/assignment/assignment-detail-read-only/assignment-detail-read-only.component.ts
@@ -15,8 +15,6 @@ export class AssignmentDetailReadOnlyComponent implements OnInit {
   simpleAssignment: SimpleAssignmentOutput;
   selectedUserStory: UserStory;
   selectedDeveloper: Developer;
-  mode ;
-  value;
   developers: Developer[];
   userStories: UserStory[];
   constructor(
@@ -25,8 +23,6 @@ export class AssignmentDetailReadOnlyComponent implements OnInit {
   ) {
     this.simpleAssignment = JSON.parse('{"assignmentErrors":[],"simpleDeveloperUserStoriesAssigned":[{"developer":{"id":788273,"available_hours_per_week":45,"full_name":"Luis Gerardo Manrique Cardona"},"userStories":[{"id":2239865,"total_points":12,"reference":1,"subject":"Permitir el logueo en el sistema basado en los usuarios de el Campus Virtual, haciendo uso de sus nombres de usuario, contraseña y rol asignado en el Campus"},{"id":2252677,"total_points":12,"reference":2,"subject":"Permitir asignaciones basadas en un conjunto de historias resueltas"}]},{"developer":{"id":8,"available_hours_per_week":45,"full_name":"Arnaldo ramirez"},"userStories":[{"id":5,"total_points":12,"reference":4,"subject":"Permitir reasignar historias de usuario"},{"id":18,"total_points":12,"reference":5,"subject":"Permitir reasignacion en tiempo  real"}]}]}');
     this.developers = new Array<Developer>();
-    this.mode = 'determinate';
-    this.value = 40;
     this.userStories = new Array<UserStory>();
   }
   totalUserStoryPoints(userStories: UserStory[]): number {
