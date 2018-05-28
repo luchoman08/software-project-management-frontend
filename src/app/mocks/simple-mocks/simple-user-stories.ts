535eba09e5bd51048f0568be8826aba3334c8409
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-05-28 10:25:28 -0500

    New mocks for simple assignmen

--- src/app/mocks/simple-mocks/simple-user-stories.ts
+++ src/app/mocks/simple-mocks/simple-user-stories.ts
@@ -4,5 +4,29 @@ export const SIMPLEUSERSTORIES: UserStory[] = [
      subject:  'Crear usuarios '},
 
      { id: '101', total_points: null, reference: 2,
-     subject:  'Editar usuarios'}
+     subject:  'Editar usuarios'},
+     {
+        id: '5',
+        total_points: 12,
+         reference: 4,
+        subject: "Permitir reasignar historias de usuario"
+      },
+      {
+        id: '18',
+        total_points: 12,
+        reference: 5,
+        subject: "Permitir reasignacion en tiempo  real"
+      },
+      {
+        id: '2239865',
+        total_points: 12,
+        reference: 1,
+        subject: "Permitir el logueo en el sistema basado en los usuarios de el Campus Virtual, haciendo uso de sus nombres de usuario, contraseña y rol asignado en el Campus"
+      },
+      {
+        id: '2252677',
+        "total_points": 12,
+         "reference": 2,
+        "subject": "Permitir asignaciones basadas en un conjunto de historias resueltas"
+      }
   ];
