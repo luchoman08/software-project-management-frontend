e620c2082f6a57e647a34657236a06516130472b
Jose Manuel Sanclemente Franco <sanclemente.jose@correounivalle.edu.co>
2018-05-28 19:53:48 -0500

    All logic for calculate developers percentage time used

--- src/app/core/models/assignment-output.model.ts
+++ src/app/core/models/assignment-output.model.ts
@@ -1,5 +1,30 @@
 import { DeveloperUserStoriesAssigned } from './';
+import { UserStory } from './user-story.model';
 export class SimpleAssignmentOutput {
     simpleDeveloperUserStoriesAssigned: DeveloperUserStoriesAssigned[];
     assignmentErrors: string [];
+    public fromJSON(json: {assignmentErrors: string [],simpleDeveloperUserStoriesAssigned:  DeveloperUserStoriesAssigned[]}){
+        this.simpleDeveloperUserStoriesAssigned = json.simpleDeveloperUserStoriesAssigned;
+        this.assignmentErrors = json.assignmentErrors;
+    }
+    private totalUserStoryPoints(userStories: UserStory[]): number {
+        let totalPoints: number = 0;
+        for(let userStory of userStories) {
+          totalPoints += userStory.total_points;
+        }
+        return totalPoints;
+      }
+    private developerPercentageOcupation(developerAssignment: DeveloperUserStoriesAssigned) {
+        console.log("una vez y otra mas hasta juntos terminar ");
+        return  this.totalUserStoryPoints(developerAssignment.userStories) * 100 / developerAssignment.developer.available_hours_per_week;
+      }
+    private initDevelopersPercentageTimeUsed(): void {
+        for (let simpleDeveloperUserStoriesAssign of this.simpleDeveloperUserStoriesAssigned) {
+            simpleDeveloperUserStoriesAssign.developer.percentage_time_used = 
+            this.developerPercentageOcupation(simpleDeveloperUserStoriesAssign);
+        }
+    }
+    public init(){
+        this.initDevelopersPercentageTimeUsed();
+    }
 }
