f0adf8e7b7efe577009029946025cd59feb30e06
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-08-07 14:20:37 -0500

    Added generate assignmet by punctuation to assignment service

--- src/app/core/services/assginment.service.ts
+++ src/app/core/services/assginment.service.ts
@@ -5,19 +5,28 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 import { ApiService } from './api.service';
 import { AssignmentInput } from '../models/assignment-input.model';
-import { SimpleAssignmentOutput } from '../models/assignment-output.model';
+import { AssignmentByPunctuation } from '../models/assignment-by-punctuations.model';
 
 @Injectable()
 export class AssignmentService {
   constructor(private apiService: ApiService) {}
 
+  generateAssignmentByPunctuations(
+    assignmentByPunctuations: AssignmentByPunctuation): Observable<AssignmentByPunctuation> {
+      return this.apiService
+      .post('/attributeassign/', assignmentByPunctuations)
+      .map((assignmentByPunctuations: AssignmentByPunctuation) => {
+        return assignmentByPunctuations;
+      })
+    }
+  )
   generarAsignacionSimple(
-    assignmentInput: AssignmentInput
+    assignmentUniqueCost: AssignmentInput
   ): Observable<AssignmentInput> {
     return this.apiService
-      .post('/uniquecostassign/', assignmentInput)
-      .map((assignmentOptut: AssignmentInput) => {
-        return assignmentOptut;
+      .post('/uniquecostassign/', assignmentUniqueCost)
+      .map((assignmentUniqueCost: AssignmentInput) => {
+        return assignmentUniqueCost;
       });
   }
 }
