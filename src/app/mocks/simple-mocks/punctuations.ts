f92d2a79293840396c75a0e14c7a670d010d487b
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-08-08 17:29:40 -0500

    Added punctuations mocks

--- src/app/mocks/simple-mocks/punctuations.ts
+++ src/app/mocks/simple-mocks/punctuations.ts
@@ -1,7 +1,12 @@
 import { Punctuation } from "../../core/models/punctuation";
 
 export const punctuations_mock = new Array<Punctuation>();
+export var punctuations_mock_inverse = new Array<Punctuation>();
+
 punctuations_mock.push({id: 0, name: 'UX', value: 12});
 punctuations_mock.push({id: 1, name: 'Front', value: 1});
 punctuations_mock.push({id: 2, name: 'Back', value: 13});
 
+punctuations_mock_inverse.push({id: 0, name: 'UX', value: 1});
+punctuations_mock_inverse.push({id: 1, name: 'Front', value: 12});
+punctuations_mock_inverse.push({id: 2, name: 'Back', value: 1});
