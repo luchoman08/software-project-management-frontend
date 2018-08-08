45f024a569d8f1a95b2d12456a9d1a96e45717d1
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-08-08 17:30:26 -0500

    Simple developers mocks updated for distinct punctuations than user stories

--- src/app/mocks/simple-mocks/simple-developers.ts
+++ src/app/mocks/simple-mocks/simple-developers.ts
@@ -1,13 +1,13 @@
 import { IDeveloper } from '../../core/models/developer.interface';
-import { punctuations_mock } from './punctuations';
+import { punctuations_mock, punctuations_mock_inverse } from './punctuations';
 export const SIMPLEDEVELS: IDeveloper[] = [
     { id: '100', full_name: 'Luis Gerardo Manrique',
      color:  '#2196F3',  available_hours_per_week:  null,
-     punctuations: punctuations_mock,
+     punctuations: punctuations_mock_inverse,
      role_name : 'Developer'},
 
      { id: '101', full_name: 'Jefferson Meneses',
      color:  'rgb(255, 87, 0)',  available_hours_per_week:  30,
-     punctuations: punctuations_mock,
+     punctuations: punctuations_mock_inverse,
      role_name : 'Developer'}
   ];
