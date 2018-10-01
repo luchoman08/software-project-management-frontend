3ffc0d2f2612be173032cf8f13eec624fe69cbaf
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-10-01 14:49:59 -0500

    Punctuations selector view children for all sliders

--- src/app/punctuations/punctuations-selector/punctuations-selector.component.ts
+++ src/app/punctuations/punctuations-selector/punctuations-selector.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { Punctuation } from '../../core/models/punctuation';
 import { punctuations_mock } from '../../mocks/simple-mocks/punctuations';
-
+import { MatSliderComponent } from '@angular/material';
 @Component({
   selector: 'app-punctuations-selector',
   templateUrl: './punctuations-selector.component.html',
@@ -9,6 +9,7 @@ import { punctuations_mock } from '../../mocks/simple-mocks/punctuations';
 })
 export class PunctuationsSelectorComponent implements OnInit {
   punctuations : Array<Punctuation> = punctuations_mock;
+  @ViewChildren(MatSliderComponent) sliders : QueryList<MatSliderComponent>;
   max = 100;
   min = 0;
   step = 1;
