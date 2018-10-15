c2b4c38d61fba5f386136e69c8411cd997470ea2
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2018-10-15 11:34:06 -0500

    feat(developer): swap developers in developer pairs complete

--- src/app/developer/developer-pair/developer-pair.component.ts
+++ src/app/developer/developer-pair/developer-pair.component.ts
@@ -10,28 +10,52 @@ import { Developer } from '../../core/models';
 })
 export class DeveloperPairComponent implements OnInit {
   pairs: Array<DeveloperPair> = PAIRS;
-  sourceDeveloper: Developer;
-  objeciveDeveloper: Developer;
+  indexSourceDeveloper: number;
+  indexObjectiveDeveloper: number;
+  indexSourcePair: number;
+  indexObjectivePair: number;
   moving: boolean ;
 
   constructor() { }
-  setSourceDeveloper(developer: Developer) {
+  setIndexSourceDeveloper(index: number, indexPair: number) {
     if (!this.moving) {
-      
-      this.sourceDeveloper = developer;
+      this.setIndexSourcePair(indexPair);
+      this.indexSourceDeveloper = index;
     }
     this.moving = true;
   }
-  setObjectiveDeveloper(developer: Developer) {
-    this.objeciveDeveloper = developer;
+  setIndexObjectiveDeveloper(index: number,  indexPair: number) {
+    this.setIndexObjectivePair(indexPair);
+    this.indexObjectiveDeveloper = index;
   }
-
+  setIndexSourcePair(index: number) {
+    if (!this.moving) {
+      this.indexSourcePair = index;
+    }
+  
+    this.moving = true;
+  }
+  setIndexObjectivePair(index: number) {
+    this.indexObjectivePair = index;
+  }
+  swapDevelopers() {
+    let sufixObjectiveDeveloper = this.indexObjectiveDeveloper + 1;
+    let sufixSourceDeveloper = this.indexSourceDeveloper + 1;
+    let sufix = 'developer';
+    let sourcePropertyName = sufix +  sufixSourceDeveloper;
+    let objectivePropertyName = sufix +  sufixObjectiveDeveloper;
+    /* Check if pair have the property names used in this method */
+    if( ! ('developer1' in this.pairs[this.indexSourcePair])  &&  ('developer2' in this.pairs[this.indexSourcePair]) ) {
+      console.error('Los desarrolladores deben tener las propiedades developer1 y developer2');
+      return;
+    } 
+    let tempDevel = this.pairs[this.indexObjectivePair][objectivePropertyName];
+    this.pairs[this.indexObjectivePair][objectivePropertyName] = this.pairs[this.indexSourcePair][sourcePropertyName];
+    this.pairs[this.indexSourcePair][sourcePropertyName] = tempDevel;
+  }
   onItemDrop(e: any) {
     // Get the dropped data here
-    let droped_developer: Developer = e.dragData;
-    console.log(this.objeciveDeveloper);
-    this.pairs[0].developer1 = droped_developer;
-
+    this.swapDevelopers();
     this.moving = false;
   }
 
