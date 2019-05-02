b45e41bb845a2f0eaefcc139c0b8af1b2d277ac8
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-02 17:36:16 -0500

    feat: to ease print developer pair concat name

--- src/app/assignment/assignment-detail-pair/assignment-detail-pair.component.ts
+++ src/app/assignment/assignment-detail-pair/assignment-detail-pair.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { AssignmentByPairs } from '../../core/models/assignment-by-pairs.model';
 import { DeveloperPair } from '../../core/models/developer-pair.model';
-import { UserStory } from '../../core/models';
+import { UserStory, Developer } from '../../core/models';
+import { ASSIGNMENT_TO_PAIRS } from '../../mocks/simple-mocks/assignment-to-pairs.mock';
+import { MatDialog } from '@angular/material';
+import { AssignedToPairPipe } from '../../core/pipes/assigned-to-pair.pipe';
+import { UserStoriesService } from '../../core/services';
+import { DeveloperSelectPairDialogComponent, dataOutput } from '../../developer/developer-select-pair-dialog/developer-select-pair-dialog.component';
 
 @Component({
   selector: 'app-assignment-detail-pair',
@@ -12,9 +17,36 @@ export class AssignmentDetailPairComponent implements OnInit {
   @Input() assignmentByPairs: AssignmentByPairs;
   selectedPair: DeveloperPair;
   selectedUserStory: UserStory;
-  constructor() { }
-
+  constructor(
+    public dialog: MatDialog,
+    private cd: ChangeDetectorRef
+  ) { }
+  openSelectPair(userStory: UserStory) {
+    this.selectedUserStory = userStory;
+    console.log(userStory, 'userStory, selected devel and pair in at open select pair')
+    console.log(this.assignmentByPairs.pairs, 'userStory, selected devel and pair in at open select pair')
+    const dialogRef = this.dialog.open(DeveloperSelectPairDialogComponent, {
+      width: '620px',
+      data: {
+        pairs: this.assignmentByPairs.pairs.filter((pair: DeveloperPair) => { 
+          return !userStory.assignedTo( pair.developer1 ) &&
+          !userStory.assignedTo( pair.developer2 ) 
+        }),
+        oldPair: this.selectedPair,
+        title: 'Seleccione la pareja de desarrolladores'
+      }
+    });
+    dialogRef.afterClosed().subscribe((dataOutput: dataOutput ) => {
+      console.log( dataOutput.selectedPair.developerIds, 'developer ids for new assign'); 
+      this.selectedUserStory.assigned_to = dataOutput.selectedPair.developerIds;
+      this.cd.markForCheck();
+    });
+  }
   ngOnInit() {
+    if (this.assignmentByPairs === undefined) {
+      this.assignmentByPairs = AssignmentByPairs.makeFromJson(ASSIGNMENT_TO_PAIRS);
+    }
   }
 
 }
+
