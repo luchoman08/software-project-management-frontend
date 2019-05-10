0fa427afc186a868aee36aea569bc4565c482e37
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-09 22:57:51 -0500

    feat: scroll to top on every route update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,67 @@
 import { Component, OnInit, ChangeDetectorRef, ViewChild, OnDestroy } from '@angular/core';
 
-import { PageComponentsService, LoadingBarService } from './core/services';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { SidenavService } from './core/services/sidenav.service';
 import { MatSidenav } from '@angular/material';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { PageComponentsService, LoadingBarService } from './core/services';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnDestroy, OnInit {
     public full_page_component: boolean;
-    @ViewChild('snav') snav: MatSidenav;
+    @ViewChild('snav') private snav: MatSidenav;
     mobileQuery: MediaQueryList;
-    fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
     private _mobileQueryListener: () => void;
-
+    private lastPoppedUrl: string;
+    private yScrollStack: number[] = [];
     ngOnDestroy(): void {
-      this.mobileQuery.removeListener(this._mobileQueryListener);
+        this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
     constructor(
         private pageComponentService: PageComponentsService,
         private sidenav: SidenavService,
-        changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-            this.mobileQuery = media.matchMedia('(max-width: 600px)');
-            this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-            this.mobileQuery.addListener(this._mobileQueryListener);
-            this.full_page_component = false;
-            this.pageComponentService
+        private router: Router,
+        private changeDetectorRef: ChangeDetectorRef,
+        private media: MediaMatcher) {
+            this.makeSidenavResponsive();
+            this.registerScrollToTopOnRouteChange();
+    }
+    private registerScrollToTopOnRouteChange() {
+        this.router.events.subscribe((event: any) => {
+            if (event instanceof NavigationStart) {
+               if (event.url !== this.lastPoppedUrl) {
+                   this.yScrollStack.push(window.scrollY);
+               }
+           } else if (event instanceof NavigationEnd) {
+               if (event.url === this.lastPoppedUrl) {
+                   this.lastPoppedUrl = undefined;
+                   window.scrollTo(0, this.yScrollStack.pop());
+               } else {
+                   window.scrollTo(0, 0);
+               }
+           }
+        });
+    }
+    private makeSidenavResponsive() {
+        this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
+        this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this._mobileQueryListener);
+        this.full_page_component = false;
+        this.pageComponentService
             .get_full_page_component_obs()
             .subscribe(
                 nextVal => {
                     this.full_page_component = nextVal;
                 }
             );
-        }
-    toggleRightSidenav() {
-        this.sidenav.toggle();
     }
     ngOnInit() {
         this.sidenav.setSidenav(this.snav);
     }
-
 }
