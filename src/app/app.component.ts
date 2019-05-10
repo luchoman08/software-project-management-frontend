bed132b572847c958879ea83f437c1ab94f06c12
Luis Gerardo Manrique Cardona <luis.manrique@correounivalle.edu.co>
2019-05-09 20:34:01 -0500

    feat: sticky header and responsive sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, ViewChild, OnDestroy } from '@angular/core';
 
 import { PageComponentsService, LoadingBarService } from './core/services';
 import { MediaMatcher } from '@angular/cdk/layout';
@@ -11,24 +11,21 @@ import { MatSidenav } from '@angular/material';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     public full_page_component: boolean;
     @ViewChild('snav') snav: MatSidenav;
     mobileQuery: MediaQueryList;
     fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-  
     private _mobileQueryListener: () => void;
-  
+
     ngOnDestroy(): void {
       this.mobileQuery.removeListener(this._mobileQueryListener);
     }
 
-
     constructor(
         private pageComponentService: PageComponentsService,
         private sidenav: SidenavService,
-        changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
-        public loader: LoadingBarService) {
+        changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
             this.mobileQuery = media.matchMedia('(max-width: 600px)');
             this._mobileQueryListener = () => changeDetectorRef.detectChanges();
             this.mobileQuery.addListener(this._mobileQueryListener);
--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SidenavService } from 'app/core/services/sidenav.service';
+import { LoadingBarService } from 'app/core/services';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,10 @@ import { SidenavService } from 'app/core/services/sidenav.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-    constructor(private sidenav: SidenavService) {
+    constructor(
+      private sidenav: SidenavService,
+      public loader: LoadingBarService
+      ) {
     }
     toggleRightSidenav() {
         this.sidenav.toggle();
